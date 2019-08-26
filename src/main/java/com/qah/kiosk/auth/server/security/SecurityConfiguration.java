dcac29a31f83a782d31d0be55d5c49d050f0adc4
natf17 <natfar.dev@gmail.com>
2019-08-26 14:02:16 -0400

    No sessions

--- src/main/java/com/qah/kiosk/auth/server/security/SecurityConfiguration.java
+++ src/main/java/com/qah/kiosk/auth/server/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -27,6 +28,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.and()
 			.csrf()
 				.ignoringAntMatchers("/token"); // this POST triggers no side effects
+		
+		http
+        .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 	
 	@Override
